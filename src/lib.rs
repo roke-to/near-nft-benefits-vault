d6deebaf3d9e1268bf5e6f5675cd0e9e6564d2aa
Pavel Iakushin <jetpilot412@gmail.com>
2022-10-27 14:58:25 +0400

    add withdraw method to get specified FT from the vault

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,28 @@ impl Contract {
         nft_info_promise.then(Self::ext(env::current_account_id()).withdraw_all_callback(nft_id))
     }
 
+    #[payable]
+    pub fn withdraw(
+        &mut self,
+        nft_contract_id: AccountId,
+        nft_id: TokenId,
+        ft_contract_id: AccountId,
+    ) -> Promise {
+        let caller = env::predecessor_account_id();
+        log!("withdraw call by {}", caller);
+        log!("check attached deposit: 1 yocto");
+        // 1 yoctoNEAR should attached to this call to prevent abuse.
+        assert_one_yocto();
+
+        let nft_id = NftId::new(nft_contract_id, nft_id);
+
+        let nft_info_promise =
+            nft::ext(nft_id.contract_id().to_owned()).nft_token(nft_id.token_id().to_owned());
+
+        nft_info_promise
+            .then(Self::ext(env::current_account_id()).withdraw_callback(nft_id, ft_contract_id))
+    }
+
     /// Callback invokes after request to the NFT contract to check ownership and grant access to the vault.
     /// Private: can be called only by this contract.
     #[private]
@@ -84,32 +106,45 @@ impl Contract {
 
         let vault = self.get_vault(&nft_id);
 
-        let mut transfer_promise: Option<Promise> = None;
-        for (ft_account_id, asset) in vault.assets.iter() {
-            log!(
-                "add transfer ft: {}, receiver: {}, amount: {}",
-                ft_account_id,
-                nft_owner,
-                asset.balance
-            );
-            let memo = Some("Nft Benefits transfer".to_string());
-            let ft_transfer_promise = ft::ext(ft_account_id.clone())
-                .with_attached_deposit(1)
-                .ft_transfer(nft_owner.clone(), U128(asset.balance), memo);
-
-            let adjust_balance = Self::ext(env::current_account_id()).adjust_balance(
+        let mut transfer_promises: Option<Promise> = None;
+        for (ft_contract_id, asset) in vault.assets.iter() {
+            let transfer_and_adjust_promise = Self::transfer_and_adjust_balance(
                 nft_id.clone(),
-                ft_account_id.clone(),
+                ft_contract_id,
+                nft_owner.clone(),
                 asset.balance,
             );
-            if let Some(promise) = transfer_promise {
-                transfer_promise = Some(promise.then(ft_transfer_promise).then(adjust_balance));
-            // @TODO can be joined with `.and()`
+            let promise = if let Some(promise) = transfer_promises {
+                promise.then(transfer_and_adjust_promise)
             } else {
-                transfer_promise = Some(ft_transfer_promise.then(adjust_balance));
-            }
+                transfer_and_adjust_promise
+            };
+            transfer_promises = Some(promise);
         }
-        transfer_promise
+        transfer_promises
+    }
+
+    #[private]
+    pub fn withdraw_callback(
+        &mut self,
+        #[callback_result] nft_info: Result<Option<Token>, PromiseError>,
+        nft_id: NftId,
+        ft_contract_id: AccountId,
+    ) -> Promise {
+        assert_self();
+        let signer = env::signer_account_id();
+        log!("withdraw_all_callback called by signer: {}", signer);
+
+        let nft_info = nft_info
+            .expect("failed to get nft info")
+            .expect("NFT info query returned nothing");
+        let nft_owner = nft_info.owner_id;
+        require!(nft_owner == signer, "vault access denied");
+
+        let vault = self.get_vault(&nft_id);
+
+        let asset = vault.assets.get(&ft_contract_id).expect("unknown asset");
+        Self::transfer_and_adjust_balance(nft_id, ft_contract_id, nft_owner, asset.balance)
     }
 
     /// Callback invokes after each FT transfer call from this contract in withdrawal process.
@@ -119,7 +154,7 @@ impl Contract {
         &mut self,
         #[callback_result] res: Result<(), PromiseError>,
         nft_id: NftId,
-        ft_account_id: AccountId,
+        ft_contract_id: AccountId,
         amount: u128,
     ) {
         assert_self();
@@ -129,12 +164,12 @@ impl Contract {
         );
         if res.is_ok() {
             let mut vault = self.get_vault(&nft_id);
-            vault.reduce_balance(ft_account_id.clone(), amount);
+            vault.reduce_balance(ft_contract_id.clone(), amount);
             self.vaults.insert(&nft_id, &vault);
             log!(
                 "withdrawal success, nft: {:?}, ft: {}, amount: {}",
                 nft_id,
-                ft_account_id,
+                ft_contract_id,
                 amount
             );
         } else {
@@ -146,16 +181,16 @@ impl Contract {
 impl Contract {
     /// Adds provided amount of tokens to the vault specified by NFT `token_id`.
     /// If there is not vault for the provided [`NftId`] then it will create the new one.
-    pub fn store(&mut self, nft_id: NftId, ft_account_id: AccountId, amount: u128) {
+    pub fn store(&mut self, nft_id: NftId, ft_contract_id: AccountId, amount: u128) {
         let mut vault = if let Some(vault) = self.vaults.get(&nft_id) {
-            log!("current balance of {}: {}", ft_account_id, amount);
+            log!("current balance of {}: {}", ft_contract_id, amount);
             vault
         } else {
             let mut vault = Vault::new();
-            vault.add_asset(ft_account_id.clone(), 0);
+            vault.add_asset(ft_contract_id.clone(), 0);
             vault
         };
-        vault.store(ft_account_id, amount);
+        vault.store(ft_contract_id, amount);
 
         log!("vault created for: {:?}", nft_id);
         self.vaults.insert(&nft_id, &vault);
@@ -167,6 +202,28 @@ impl Contract {
         self.vaults
             .get(nft_id)
             .expect("vault is not created for the given nft_id")
+    }
+
+    pub fn transfer_and_adjust_balance(
+        nft_id: NftId,
+        ft_contract_id: AccountId,
+        nft_owner: AccountId,
+        amount: u128,
+    ) -> Promise {
+        log!(
+            "add transfer ft: {}, receiver: {}, amount: {}",
+            ft_contract_id,
+            nft_owner,
+            amount
+        );
+        let memo = Some("Nft Benefits transfer".to_string());
+        let ft_transfer_promise = ft::ext(ft_contract_id.clone())
+            .with_attached_deposit(1)
+            .ft_transfer(nft_owner, U128(amount), memo);
+
+        let adjust_balance =
+            Self::ext(env::current_account_id()).adjust_balance(nft_id, ft_contract_id, amount);
+        ft_transfer_promise.then(adjust_balance)
     }
 }
 
