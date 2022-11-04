16874138ce6df560af76a948f1625618829be6f1
Pavel Iakushin <jetpilot412@gmail.com>
2022-11-04 11:44:19 +0400

    reuse vault_deposit method of Environment

--- src/tests/withdraw.rs
+++ src/tests/withdraw.rs
@@ -18,7 +18,7 @@ async fn test_withdraw_single_ft() -> Result<()> {
     println!("\n<--- nft owner initial balances: {nft_owner_initial_balances:?} --->\n");
 
     for contract_id in env.fungible_tokens.iter().map(Contract::id) {
-        env.deposit_to_vault(contract_id).await?;
+        env.vault_deposit(contract_id).await?;
         println!("\ndeposit to vault of {contract_id}");
     }
     println!("\n<--- deposited to vault --->\n");
